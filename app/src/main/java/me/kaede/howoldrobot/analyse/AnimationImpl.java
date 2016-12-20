b82b80d06e09418db892845b5547d21deb8bda46
Kaede <kidhaibara@gmail.com>
2016-12-20 21:18:48 +0800

    opt ui

--- app/src/main/java/me/kaede/howoldrobot/analyse/AnimationImpl.java
+++ app/src/main/java/me/kaede/howoldrobot/analyse/AnimationImpl.java
@@ -7,6 +7,7 @@ package me.kaede.howoldrobot.analyse;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.AnimationSet;
+import android.view.animation.LinearInterpolator;
 import android.view.animation.TranslateAnimation;
 
 import me.kaede.howoldrobot.analyse.presenter.IAnimation;
@@ -17,12 +18,14 @@ class AnimationImpl implements IAnimation {
     public void doLogoAnimation(View view) {
         AnimationSet animationSet = new AnimationSet(false);
         AlphaAnimation alphaAnimation = new AlphaAnimation(0, 1);
-        alphaAnimation.setDuration(1000);
+        alphaAnimation.setDuration(500);
         alphaAnimation.setFillAfter(true);
+        // alphaAnimation.setInterpolator(new LinearInterpolator());
 
         TranslateAnimation translateAnimation = new TranslateAnimation(0f, 0f, 100f, 0f);
-        translateAnimation.setDuration(1000);
+        translateAnimation.setDuration(500);
         translateAnimation.setFillAfter(true);
+        // translateAnimation.setInterpolator(new LinearInterpolator());
 
         animationSet.addAnimation(alphaAnimation);
         animationSet.addAnimation(translateAnimation);
